671e075488e43bcc443dd1a58c1122c3cdc63fde
Tom <tomlambert1997@gmail.com>
2024-08-26 16:31:57 +0100

    import and create token variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import { SearchBar } from './components/SearchBar';
 import { SearchResults } from './components/SearchResults';
 import { Playlist } from './components/Playlist';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const clientID = "36e3f70fe51742af911d9af20b443a96"
+  const clientSecret = "86e0c43c5ed64fedaba801f0a43d043e"
+
+  const [token, setToken] = useState('')
+
+  useEffect(() => {
+    fetch('https://accounts.spotify.com/api/token',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`
+      }).then(response => response.json()).catch(console.log("unfullfilled")).then(data => {
+        setToken(data.access_token)
+      })
+  }, [])
+
+  console.log(token)
+
   return (
     <>
       <header>
