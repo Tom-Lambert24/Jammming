f04280a0a9f67cf4f30863153f9a548c43675967
Tom <tomlambert1997@gmail.com>
2024-08-26 11:14:33 +0100

    add results component on the left side

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { SearchBar } from './components/SearchBar';
+import { SearchResults } from './components/SearchResults';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       </header>
       <body>
         <SearchBar />
+        <SearchResults />
       </body>
     </>
   );
---
+++ src/components/SearchResults.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function SearchResults() {
+
+    return (
+        <div id="searchResults">
+            <h2>Results</h2>
+        </div>
+    )
+}
