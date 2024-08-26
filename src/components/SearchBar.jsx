16630bce4645d5962f3f9b18bead220a57bcf23c
Tom <tomlambert1997@gmail.com>
2024-08-26 18:44:25 +0100

    beginning search GET

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export function SearchBar() {
 
+    const [search, setSearch] = useState()
 
+    function changeHandler() {
+        fetch('')
+    }
 
 
     return (
         <>
             <div id="search">
-                <input id="searchInput" type="text" />
+                <input id="searchInput" type="text" onChange={changeHandler}/>
                 <br></br>
                 <button>Search</button>
             </div>
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function SearchResults(props) {
 
-    
+
 
 
     return (
