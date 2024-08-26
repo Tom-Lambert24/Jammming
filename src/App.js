a21dec0d9ef79cfc4a9c666cf1c892a48ba9bb74
Tom <tomlambert1997@gmail.com>
2024-08-26 10:53:49 +0100

    moved components into src and started editing SearchBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { searchBar } from './components/SearchBar';
 
 function App() {
   return (
     <>
       <header>
         <h1>Jammming</h1>
+        <searchBar />
       </header>
     </>
   );
